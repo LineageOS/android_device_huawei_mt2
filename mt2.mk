b0892bf9a4116960828ebf331855b0f031def69e
Tom Marshall <tdm@cyngn.com>
2015-01-13 12:45:07 -0800

    Rearrange some init stuff and add in stock changes from init.rc

--- mt2.mk
+++ mt2.mk
@@ -56,6 +56,7 @@ PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/rootdir/init.qcom.usb.rc:root/init.qcom.usb.rc \
 	$(LOCAL_PATH)/rootdir/init.qcom.usb.sh:root/init.qcom.usb.sh \
 	$(LOCAL_PATH)/rootdir/init.recovery.qcom.rc:root/init.recovery.qcom.rc \
+	$(LOCAL_PATH)/rootdir/init.target.rc:root/init.target.rc \
 	$(LOCAL_PATH)/rootdir/ueventd.qcom.rc:root/ueventd.qcom.rc \
 	$(LOCAL_PATH)/rootdir/ueventd.rc:root/ueventd.rc
 
