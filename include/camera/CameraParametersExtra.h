b2a981ef20a61765592883956745f1aa063bc8fc
Tom Marshall <tdm@cyngn.com>
2015-01-13 15:30:40 -0800

    Extra camera parameters for hal linkage

---
+++ include/camera/CameraParametersExtra.h
@@ -0,0 +1,26 @@
+// Overload this file in your device specific config if you need
+// to add extra camera parameters.
+// A typical file would look like this:
+/*
+ * Copyright (C) 2014 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#define CAMERA_PARAMETERS_EXTRA_C \
+const char CameraParameters::KEY_MULTIFOCUS_SHOT_MODE[] = "is_multifocus_mode"; \
+const char CameraParameters::SCENE_MODE_HWAUTO[] = "hwauto";
+
+#define CAMERA_PARAMETERS_EXTRA_H \
+    static const char KEY_MULTIFOCUS_SHOT_MODE[]; \
+    static const char SCENE_MODE_HWAUTO[];
