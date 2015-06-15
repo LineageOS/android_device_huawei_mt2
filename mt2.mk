a660035d330c832b00587f91db0bb5a38a7e2b5c
Jurica Vukadin <u.ra604@gmail.com>
2015-06-15 14:01:38 +0200

    mt2: Add basic mixer paths

--- mt2.mk
+++ mt2.mk
@@ -87,7 +87,8 @@ PRODUCT_PACKAGES += \
 # Audio config files
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
-    $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml
+    $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
+    $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
 
 # WiFi
 PRODUCT_COPY_FILES += \
