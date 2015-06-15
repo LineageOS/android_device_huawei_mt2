22d6de290676554aad54eb45fe6172f0714e858d
Jurica Vukadin <u.ra604@gmail.com>
2015-06-15 16:47:20 +0200

    mt2: Build QCOM random number daemon

--- mt2.mk
+++ mt2.mk
@@ -95,6 +95,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
     $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml
 
+# QCOM RNG
+PRODUCT_PACKAGES += \
+    qrngd \
+    qrngp
 # WiFi
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
