1d3ba53d808673c62fb2d8fb286d1f3817fbd783
Jurica Vukadin <u.ra604@gmail.com>
2015-06-30 22:48:47 +0200

    mt2: Use A7 memcpy for dem speed gainz

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -28,6 +28,7 @@ TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_VARIANT := krait
+TARGET_CPU_MEMCPY_BASE_OPT_DISABLE := true
 
 # Audio
 BOARD_USES_ALSA_AUDIO := true
