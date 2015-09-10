2e8c206899c0f9784b58113ad399f76b3bd7b8bd
Matt Mower <mowerm@gmail.com>
2015-09-10 13:42:09 -0500

    mt2: Provide global Huawei symbols via libc
    Huawei included some non-standard functions in libcutils and liblog.
    Make these functions globally available by including them in libc.
    
    Parts of this commit originated from:
      libcutils/liblog: Add functions required by Huawei g620_a2 binaries
      Thomas Wendt
      I58604686008340ef4e10f3f67dc329fe7bd864cc
    
    Change-Id: Ie3a84ce5b6b969ef4cc17b2028cc03298a43a79b

---
+++ libc_huawei_symbols/hw_cutils.c
@@ -0,0 +1,66 @@
+/*
+ * Copyright (C) 2015 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#define LOG_TAG "HWAppInfo"
+
+#include <log/log.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+#define APP_INFO "/proc/app_info"
+#define APP_INFO_VALUE_LENTH 32
+
+int get_app_info(char* key, char* value) {
+    char buf[128] = { 0 };
+    char* tok;
+    FILE* f;
+
+    ALOGI("Getting App Info for %s", key);
+
+    if(key == NULL) {
+        ALOGE("Key is null");
+        return -1;
+    }
+
+    f = fopen(APP_INFO, "rb");
+    if(f == NULL) {
+        ALOGE("Failed to open %s: %s", APP_INFO, strerror(errno));
+        return -2;
+    }
+
+    while (!feof(f)) {
+        if (fgets(buf, 128, f) != NULL &&
+                strstr(buf, key) != NULL) {
+            tok = strchr(buf, ':');
+            if (tok != NULL)
+                tok = strtok(tok, ": ");
+            if (tok != NULL) {
+                snprintf(value, APP_INFO_VALUE_LENTH, "%s", tok);
+                strtok(value, "\n");
+            }
+            break;
+        }
+    }
+
+    fclose(f);
+
+    ALOGI("%s=%s", key, value);
+
+    return 0;
+}
