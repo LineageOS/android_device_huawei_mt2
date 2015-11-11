9f3bbb4b9979602f4e0becd68c255b415b42ec4f
Steve Kondik <steve@cyngn.com>
2015-11-11 17:49:51 +0100

    mt2: Build the kernel with gcc-4.9
    Change-Id: I8f52dfd4776492f5167bc3096103d5794bf9fa14

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -89,6 +89,7 @@ BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_SEPARATED_DT := true
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000
 TARGET_KERNEL_CONFIG := cyanogenmod_mt2_defconfig
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-linux-androideabi-
 TARGET_KERNEL_SOURCE := kernel/huawei/msm8928
 
 # Lights
