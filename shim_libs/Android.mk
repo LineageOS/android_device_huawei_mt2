54cee3e080f79aeb535b671f6c274cb98741ff90
Jurica Vukadin <u.ra604@gmail.com>
2015-11-25 23:09:37 +0100

    mt2: Fix makefile derp
    Change-Id: Id61e9cf884c4f8d2f7a7d84b990684db25253573

--- shim_libs/Android.mk
+++ shim_libs/Android.mk
@@ -43,7 +43,7 @@ LOCAL_SHARED_LIBRARIES := liblog libcutils libgui libbinder libutils
 LOCAL_ADDITIONAL_DEPENDENCIES := libboringssl-compat
 
 LOCAL_SRC_FILES := \
-    hw_camera.c
+    hw_gui.c
 
 LOCAL_MODULE := libhw_gui
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
